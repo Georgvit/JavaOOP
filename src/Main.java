13521372d9d968d6343b37c9794b1b07811c7f80
Georgvit <ifreyd@mail.ru>
2022-12-14 23:20:58 +0300

    первая лекция ООП

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Product water = new Product("Soda", 34.4);
+//        System.out.println(water);
+
+        VendingMachine machineOne = new VendingMachine();
+        System.out.println(machineOne);
+
+        System.out.println();
+
+        List<Product> merchandise = new ArrayList<>();
+        merchandise.add(water);
+        merchandise.add(new Product("wine", 400));
+        merchandise.add(new Product("cheese", 23.7));
+        merchandise.add(new Product("meet", 13.7));
+        merchandise.add(new SodaWater("Cola", 1.2, "coll"));
+
+        VendingMachine machineTwo = new VendingMachine(merchandise);
+        System.out.println(machineTwo);
+        System.out.println();
+        System.out.println(machineTwo.filterName("meet"));
+        System.out.println();
+        System.out.println(machineTwo.filterPriceRange(13, 30));
+        System.out.println();
+        System.out.println(machineTwo.filterPrice(13.7));
+
+
     }
 }
---
+++ src/Product.java
@@ -0,0 +1,37 @@
+public class Product {
+    //    Создание полей класса
+    private String name;
+    private double price;
+
+    //  Создаем конструктор с параметрами
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    //    Создаем сеттеры и геттеры
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    //    Переопределяем toString
+    @Override
+    public String toString() {
+        return "\n" + "Product " +
+                "name = '" + name + '\'' +
+                ", price = " + price +
+                " руб.";
+    }
+}
